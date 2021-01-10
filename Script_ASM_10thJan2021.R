2c3b3550d17bc4199f836916786772ef09dd989f
Akul Suhail Malhotra <am5376a@student.american.edu>
2021-01-10 12:16:30 -0500

    Update and rename Script_AK_6thJan2020.R to Script_ASM_10thJan2021.R

--- Script_ASM_10thJan2021.R
+++ Script_ASM_10thJan2021.R
@@ -67,3 +67,75 @@ data_deceased_sub <- data_deceased %>%
   pivot_longer(names_to = "date", cols = 5:ncol(data_deceased)) %>%
   group_by(`Province/State`, `Country/Region`, date, Lat, Long) %>%
   summarise("deceased" = sum(value, na.rm = T))
+
+
+
+
+
+##################### Malhotra_AkulSuhail_01/10/2021 ########################
+
+# US States
+data_confirmed_sub_us <- data_confirmed_us %>%
+  select(Province_State, Country_Region, Lat, Long_, 12:ncol(data_confirmed_us)) %>%
+  rename(`Province/State` = Province_State, `Country/Region` = Country_Region, Long = Long_) %>%
+  pivot_longer(names_to = "date", cols = 5:(ncol(data_confirmed_us) - 7)) %>%
+  group_by(`Province/State`, `Country/Region`, date) %>%
+  mutate(
+    Lat  = na_if(Lat, 0),
+    Long = na_if(Long, 0)
+  ) %>%
+  summarise(
+    "Lat"       = mean(Lat, na.rm = T),
+    "Long"      = mean(Long, na.rm = T),
+    "confirmed" = sum(value, na.rm = T)
+  )
+
+data_deceased_sub_us <- data_deceased_us %>%
+  select(Province_State, Country_Region, 13:(ncol(data_confirmed_us))) %>%
+  rename(`Province/State` = Province_State, `Country/Region` = Country_Region) %>%
+  pivot_longer(names_to = "date", cols = 5:(ncol(data_deceased_us) - 11)) %>%
+  group_by(`Province/State`, `Country/Region`, date) %>%
+  summarise("deceased" = sum(value, na.rm = T))
+
+data_us <- data_confirmed_sub_us %>%
+  full_join(data_deceased_sub_us) %>%
+  add_column(recovered = NA) %>%
+  select(`Province/State`, `Country/Region`, date, Lat, Long, confirmed, recovered, deceased)
+
+data_evolution <- data_confirmed_sub %>%
+  full_join(data_recovered_sub) %>%
+  full_join(data_deceased_sub) %>%
+  rbind(data_us) %>%
+  ungroup() %>%
+  mutate(date = as.Date(date, "%m/%d/%y")) %>%
+  arrange(date) %>%
+  group_by(`Province/State`, `Country/Region`, Lat, Long) %>%
+  fill(confirmed, recovered, deceased) %>%
+  replace_na(list(deceased = 0, confirmed = 0)) %>%
+  mutate(
+    recovered_est = lag(confirmed, 14, default = 0) - deceased,
+    recovered_est = ifelse(recovered_est > 0, recovered_est, 0),
+    recovered     = coalesce(recovered, recovered_est),
+    active        = confirmed - recovered - deceased
+  ) %>%
+  select(-recovered_est) %>%
+  pivot_longer(names_to = "var", cols = c(confirmed, recovered, deceased, active)) %>%
+  filter(!(is.na(`Province/State`) && `Country/Region` == "US")) %>%
+  filter(!(Lat == 0 & Long == 0)) %>%
+  ungroup()
+
+# Calculating new cases
+data_evolution <- data_evolution %>%
+  group_by(`Province/State`, `Country/Region`) %>%
+  mutate(value_new = value - lag(value, 4, default = 0)) %>%
+  ungroup()
+
+rm(data_confirmed, data_confirmed_sub, data_recovered, data_recovered_sub, data_deceased, data_deceased_sub,
+   data_confirmed_sub_us, data_deceased_sub_us)
+
+
+
+
+
+
+
